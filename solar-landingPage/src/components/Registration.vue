db687e2cae9b8ffd5b8ce56ee8095557988dba63
ollatomiwa <ajaomoshood@gmail.com>
2024-11-22 23:13:54 +0100

    Registration page update

--- solar-landingPage/src/components/Registration.vue
+++ solar-landingPage/src/components/Registration.vue
@@ -1,5 +1,61 @@
 <template>
-    <div>
-        <h3>REGISTRATION</h3>
+    <div class="container">
+        <form action="" class="login">
+            <div class="column">
+                <div class="header">
+                    
+                </div>
+                <div class="form">
+                    <h1>Welcome Back</h1>
+                    <div class="form-page">
+                        <div class="segmented">
+                            <button class="segmented-btn " aria-selected="true">
+                                Sign In
+                            </button>
+                            <button class="segmented-btn">
+                                Sign Up
+                            </button>
+                            <div class="segmented-focus"></div>
+                        </div>
+                        <div class="field">
+                            <input class="input-textbox" type="email" name="email">
+                            <label for="user-email" class="label">
+                                Email
+                            </label>
+                        </div>
+                        <button class="btn" type="submit">Continue</button>
+                        <div class="or">Or continue with</div>
+                        <div class="socia">
+                            <button class="social-btn">
+                                <i class="fa-brands fa-google"></i>
+                            </button>
+                            <button class="social-btn">
+                                <i class="fa-brands fa-apple"></i>
+                            </button>
+                            <button class="social-btn">
+                                <i class="fa-brands fa-github"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer">
+                    <ul class="footer-nav">
+                        <li><a href="">Terms of Us</a></li>
+                        <li><a href="#">Disclosures</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column column--bg">
+                <img src="" alt="" class="bg-img">
+            </div>
+        </form>
+    
     </div>
 </template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>

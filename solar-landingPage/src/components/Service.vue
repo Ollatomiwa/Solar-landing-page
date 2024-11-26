7dd95ec5c85987c9207da8e7f49423cc6cb037ec
Tomiwa <ajaomoshood@gmail.com>
2024-11-26 16:59:32 +0100

    update to service page

--- solar-landingPage/src/components/Service.vue
+++ solar-landingPage/src/components/Service.vue
@@ -1,5 +1,114 @@
 <template>
-    <div>
-        <h2>HELLO WORLD</h2>
-    </div>
+    <!--BLOG-->
+    <section id="blog" class="full-height py-3 px-lg-5 text-white bg-info">
+        <div class="container">
+
+            <div class="row pb-4 py-3 justify-content-end ">
+                <div class="col-lg-8 ">
+                    
+                    <h2>LATEST PRODUCTS</h2>
+                </div>
+            </div>
+            <div class="row gy-4">
+                <div class="col-md-4">
+                    <div class="card-custom rounded-4 bg-base shadow-effect">
+                        <div class="card-custom-image rounded-4">
+                            <img class="rounded-4" src="./assets/" alt="">
+                        </div>
+                        <div class="card-custom-content p-3">
+                            <h4>
+                                SOLAR PANEL V1.6 
+                            </h4> 
+                            <p class="text-brand mb-2">$400</p>
+                            <h5 class="mb-4"> Solar Energy Panels</h5>
+                            <a href="#" class="link-custom">Link not available yet</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card-custom rounded-4 bg-base shadow-effect">
+                        <div class="card-custom-image rounded-4">
+                            <img class="rounded-4" src="./assets/" alt="">
+                        </div>
+                        <div class="card-custom-content p-3">
+                            <h4>
+                                SOLAR PANEL V2.98
+                            </h4>
+                            <p class="text-brand mb-2">$450</p>
+                            <h5 class="mb-4">SOLAR PANEL V1.89 </h5>
+                            <a href="#" class="link-custom">Link not available yet</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card-custom rounded-4 bg-base shadow-effect">
+                        <div class="card-custom-image rounded-4">
+                            <img class="rounded-4" src="./assets/" alt="">
+                        </div>
+                        <div class="card-custom-content p-3">
+                            <h4>
+                                SOLAR PANEL V1.90
+                            </h4>
+                            <p class="text-brand mb-2">SOLAR PANEL V1.90</p>
+                            <h5 class="mb-4">SOLAR PANEL V1.90</h5>
+                            <a href="#" class="link-custom">Link not available yet</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card-custom rounded-4 bg-base shadow-effect">
+                        <div class="card-custom-image rounded-4">
+                            <img class="rounded-4" src="./assets/img/" alt="">
+                        </div>
+                        <div class="card-custom-content p-3">
+                            <h4>
+                                BATTERY 1.78V
+                            </h4>
+                            <p class="text-brand mb-2">$300</p>
+                            <h5 class="mb-4">Solar Energy Battery </h5>
+                            <a href="#" class="link-custom">Link not available yet</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card-custom rounded-4 bg-base shadow-effect">
+                        <div class="card-custom-image rounded-4">
+                            <img class="rounded-4" src="./assets/img/" alt="">
+                        </div>
+                        <div class="card-custom-content p-3">
+                            <h4>
+                                BATTERY 5.78V
+                            </h4>
+                            <p class="text-brand mb-2">$500</p>
+                            <h5 class="mb-4">500W capacity battery </h5>
+                            <a href="#" class="link-custom">Link not available yet</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card-custom rounded-4 bg-base shadow-effect">
+                        <div class="card-custom-image rounded-4">
+                            <img class="rounded-4" src="./assets/img/" alt="">
+                        </div>
+                        <div class="card-custom-content p-3">
+                            <h4>
+                               INVERTER 5.6V
+                            </h4>
+                            <p class="text-brand mb-2">$500</p>
+                            <h5 class="mb-4">Solar Energy Inverter</h5>
+                            <a href="#" class="link-custom">Read More</a>
+                        </div>
+                    </div>
+    
+                </div>
+            </div>
+           
+            
+        </div>
+      </section>
+      <!--//BLOG-->
 </template>
+
+<script setup>
+
+</script>

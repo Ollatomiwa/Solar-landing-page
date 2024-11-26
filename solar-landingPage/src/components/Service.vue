715c5187338c42dcab800f63a94f1f0c1d1a3a74
ollatomiwa <ajaomoshood@gmail.com>
2024-11-26 19:24:27 +0100

    card component changed in service, page responsive to all screen sizes too.

--- solar-landingPage/src/components/Service.vue
+++ solar-landingPage/src/components/Service.vue
@@ -1,114 +1,94 @@
 <template>
     <!--BLOG-->
-    <section id="blog" class="full-height py-3 px-lg-5 text-white bg-info">
-        <div class="container">
-
-            <div class="row pb-4 py-3 justify-content-end ">
-                <div class="col-lg-8 ">
-                    
-                    <h2>LATEST PRODUCTS</h2>
-                </div>
+    <div class="container text-center ">
+        <div class="row justify-content-md-center">
+            <div class="col col-md-6 py-5  ">
+                <h2 class="text-white">LATEST PRODUCTS</h2>
+                
             </div>
-            <div class="row gy-4">
-                <div class="col-md-4">
-                    <div class="card-custom rounded-4 bg-base shadow-effect">
-                        <div class="card-custom-image rounded-4">
-                            <img class="rounded-4" src="./assets/" alt="">
-                        </div>
-                        <div class="card-custom-content p-3">
-                            <h4>
-                                SOLAR PANEL V1.6 
-                            </h4> 
-                            <p class="text-brand mb-2">$400</p>
-                            <h5 class="mb-4"> Solar Energy Panels</h5>
-                            <a href="#" class="link-custom">Link not available yet</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card-custom rounded-4 bg-base shadow-effect">
-                        <div class="card-custom-image rounded-4">
-                            <img class="rounded-4" src="./assets/" alt="">
-                        </div>
-                        <div class="card-custom-content p-3">
-                            <h4>
-                                SOLAR PANEL V2.98
-                            </h4>
-                            <p class="text-brand mb-2">$450</p>
-                            <h5 class="mb-4">SOLAR PANEL V1.89 </h5>
-                            <a href="#" class="link-custom">Link not available yet</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card-custom rounded-4 bg-base shadow-effect">
-                        <div class="card-custom-image rounded-4">
-                            <img class="rounded-4" src="./assets/" alt="">
-                        </div>
-                        <div class="card-custom-content p-3">
-                            <h4>
-                                SOLAR PANEL V1.90
-                            </h4>
-                            <p class="text-brand mb-2">SOLAR PANEL V1.90</p>
-                            <h5 class="mb-4">SOLAR PANEL V1.90</h5>
-                            <a href="#" class="link-custom">Link not available yet</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card-custom rounded-4 bg-base shadow-effect">
-                        <div class="card-custom-image rounded-4">
-                            <img class="rounded-4" src="./assets/img/" alt="">
-                        </div>
-                        <div class="card-custom-content p-3">
-                            <h4>
-                                BATTERY 1.78V
-                            </h4>
-                            <p class="text-brand mb-2">$300</p>
-                            <h5 class="mb-4">Solar Energy Battery </h5>
-                            <a href="#" class="link-custom">Link not available yet</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card-custom rounded-4 bg-base shadow-effect">
-                        <div class="card-custom-image rounded-4">
-                            <img class="rounded-4" src="./assets/img/" alt="">
-                        </div>
-                        <div class="card-custom-content p-3">
-                            <h4>
-                                BATTERY 5.78V
-                            </h4>
-                            <p class="text-brand mb-2">$500</p>
-                            <h5 class="mb-4">500W capacity battery </h5>
-                            <a href="#" class="link-custom">Link not available yet</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card-custom rounded-4 bg-base shadow-effect">
-                        <div class="card-custom-image rounded-4">
-                            <img class="rounded-4" src="./assets/img/" alt="">
-                        </div>
-                        <div class="card-custom-content p-3">
-                            <h4>
-                               INVERTER 5.6V
-                            </h4>
-                            <p class="text-brand mb-2">$500</p>
-                            <h5 class="mb-4">Solar Energy Inverter</h5>
-                            <a href="#" class="link-custom">Read More</a>
-                        </div>
-                    </div>
-    
-                </div>
-            </div>
-           
-            
         </div>
-      </section>
+    </div>    
+<div class="row">
+    <div class="col-sm-6">
+        <div class="card my-3">
+            <div class="card-body">
+                <img src="../assets/img/wind.JPG" alt="Solar Image">
+                 <h5 class="card-title">Windmill</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Check sample</a>
+            </div>
+         </div>
+    </div>
+     <div class="col-sm-6">
+        <div class="card my-3">
+            <div class="card-body">
+                <img src="../assets/img/wind.JPG" alt="Solar Image">
+                <h5 class="card-title">Winmill Fan</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Check samples</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card my-3">
+            <div class="card-body">
+                <img src="../assets/img/inverter.JPG" alt="Solar Image">
+                 <h5 class="card-title">Inverter</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Check sample</a>
+            </div>
+         </div>
+    </div>
+     <div class="col-sm-6">
+        <div class="card my-3">
+            <div class="card-body">
+                <img src="../assets/img/battery2.JPG" alt="Solar Image">
+                <h5 class="card-title">Batery 15KV</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Check sample</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card my-3">
+            <div class="card-body">
+                <img src="../assets/img/battery.JPG" alt="Solar Image">
+                 <h5 class="card-title">battery 16KV</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Check sample</a>
+            </div>
+         </div>
+    </div>
+     <div class="col-sm-6">
+        <div class="card my-3">
+        <div class="card-body">
+        <img src="../assets/img/solarpanelsrvice.JPG" alt="Solar Image">
+        <h5 class="card-title">Solar Panel</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Check sample</a>
+    </div>
+</div>
+
+    </div>
+    
+
+</div>
       <!--//BLOG-->
 </template>
 
 <script setup>
 
 </script>
+
+<style scoped>
+ .card-body img {
+    max-width: 100%; /* Ensures the image does not exceed the card's width */
+    max-height: 100%; /* Ensures the image does not exceed the card's height */
+    object-fit:fill; /* Maintains the aspect ratio without stretching */
+    display: block; /* Removes inline spacing issues */
+    margin: 0 auto; /* Optional: Centers the image horizontally */
+}
+
+
+
+</style>

03057421be1c9f5f47e1edeb9b9bcd54b5c1ea11
Tomiwa <ajaomoshood@gmail.com>
2024-11-28 13:51:03 +0100

    minor update

--- solar-landingPage/src/components/Home.vue
+++ solar-landingPage/src/components/Home.vue
@@ -5,12 +5,24 @@
 <template>
     <div class="container text-center ">
         <div class="row justify-content-md-center">
-            <div class="col col-md-6 my-5 py-5  ">
-                <h2 class="text-white">Creating a Sustainable Future with Renewable Energy Solutions</h2>
-                <p class="text-light">Discover the amazing way solar energy can transform your home or business</p>
+            <div class=" col col-md-8   ">
+                <h2 class="text-white">CREATING A SUSTAINABLE FUTURE WITH RENEWABLE ENERGY SOLUTIONS</h2>
+                <h6 class=" text-subject text-light">DISCOVER THE MANY AMAZING WAYS SOLAR ENERGY POWER CAN TRANSFORM YOUR 
+                    HOME OR BUSINESS 
+                </h6>
             </div>
         </div>
-        <button type="button" class="btn btn-warning btn-lg text-center ">Get A Free Quote</button>
+        <RouterLink to="/contact" class="btn btn-warning btn-lg text-center ">GET A FREE QUOTE</RouterLink>
+        
     </div>
 </template>
 
+<style scoped>
+    .container {
+        margin-top: 15%;
+    }
+    .text-subject {
+        margin-top:3%;
+        margin-bottom: 4%;
+    }
+</style>

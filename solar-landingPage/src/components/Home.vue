6e8cf5ea297a855a02fd865e360651ef0d9c3342
ollatomiwa <ajaomoshood@gmail.com>
2024-12-02 22:50:09 +0100

    update to home page

--- solar-landingPage/src/components/Home.vue
+++ solar-landingPage/src/components/Home.vue
@@ -1,4 +1,10 @@
 <script setup>
+    import { ref } from 'vue';
+
+    import photo from '../assets/img/manworking.jpg';
+
+
+    const windmill = ref(photo);
 
 </script>
 
@@ -13,16 +19,42 @@
             </div>
         </div>
         <RouterLink to="/contact" class="btn btn-warning btn-lg text-center ">GET A FREE QUOTE</RouterLink>
-        
     </div>
+    <div class="row justify-content-center">
+    <div class="col-sm-6">
+        <div class="card my-3">
+            <div class="card-body">
+                <div class="d-flex align-items-center">
+                    <!-- Image Section -->
+                    <img :src="windmill" alt="Solar Windmill" class="bg-dark me-3" style="width: 40%; height: auto; border-radius: 5px; 
+                        object-fit: cover; border-radius: 5%;">
+                    <!-- Text Section -->
+                    <div>
+                        <h5 class="card-title text-center">Windmill</h5>
+                        <p class="card-text text-center">With supporting text below as a natural lead-in to additional content.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 </template>
 
 <style scoped>
     .container {
-        margin-top: 15%;
+        margin-top: 13%;
     }
     .text-subject {
         margin-top:3%;
         margin-bottom: 4%;
+    }
+
+    .card {
+        border-radius: 2%;
+        border-style: groove;
     }
 </style>
